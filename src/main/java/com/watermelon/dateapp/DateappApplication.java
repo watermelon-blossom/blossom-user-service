8b7bc8a24933bbf5c234f6ddcc8cb3ddd1dcf92b
HanSeoJin <yp6484@gmail.com>
2024-05-26 15:06:28 +0900

    add project

---
+++ src/main/java/com/watermelon/dateapp/DateappApplication.java
@@ -0,0 +1,13 @@
+package com.watermelon.dateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateappApplication.class, args);
+	}
+
+}
