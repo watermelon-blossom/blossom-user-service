b42663df60996f06c02617afad204e6dd8a61d16
HanSeoJin <yp6484@gmail.com>
2024-05-26 15:06:28 +0900

    add project

---
+++ src/test/java/com/watermelon/dateapp/DateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watermelon.dateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
