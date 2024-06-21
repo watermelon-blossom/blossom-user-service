05d1477d567ec5adf3cdf0d12c9d8683737cc6fb
HanSeoJin <yp6484@gmail.com>
2024-06-21 17:42:14 +0900

    Long 타입 @NotNull로 수정

--- src/main/java/com/watermelon/dateapp/controller/UserResponse.java
+++ src/main/java/com/watermelon/dateapp/controller/UserResponse.java
@@ -2,9 +2,10 @@ package com.watermelon.dateapp.controller;
 
 import com.watermelon.dateapp.domain.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UserResponse(
-        @NotBlank(message = "생성된 유저의 ID는 공백일 수 없습니다.")
+        @NotNull(message = "생성된 유저의 ID는 공백일 수 없습니다.")
         Long id,
         @NotBlank(message = "생성된 유저의 이름은 공백일 수 없습니다.")
         String userName
