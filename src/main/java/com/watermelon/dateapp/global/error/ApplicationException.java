fa6b30a372bdc20d5ac1eee78a09968eb57e993c
양준혁 <highestbright98@naver.com>
2024-06-24 21:59:31 +0900

    [KAN-75]: refactoring - error - cause 구문 제거

--- src/main/java/com/watermelon/dateapp/global/error/ApplicationException.java
+++ src/main/java/com/watermelon/dateapp/global/error/ApplicationException.java
@@ -6,13 +6,11 @@ import lombok.Getter;
 public class ApplicationException extends RuntimeException {
 	private final ErrorType errorType;
 	private final Object data;
-	private final Throwable cause;
 
 	public ApplicationException(ErrorType errorType, Object data, Throwable cause) {
 		super(errorType.getMessage(), cause);
 		this.errorType = errorType;
 		this.data = data;
-		this.cause = cause;
 	}
 
 	public ApplicationException(ErrorType errorType) {
