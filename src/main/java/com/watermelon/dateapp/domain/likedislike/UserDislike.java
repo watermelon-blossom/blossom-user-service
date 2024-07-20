edae255afbc95fa33280fa431a21a1ce59776daf
HanSeoJin <yp6484@gmail.com>
2024-07-20 17:25:29 +0900

    like dislike entity 설계

---
+++ src/main/java/com/watermelon/dateapp/domain/likedislike/UserDislike.java
@@ -0,0 +1,24 @@
+package com.watermelon.dateapp.domain.likedislike;
+
+import com.watermelon.dateapp.domain.user.User;
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"from_user_id", "to_user_id"})
+})
+public class UserDislike {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "from_user_id")
+    private User fromUserId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "to_user_id")
+    private User toUserId;
+}
